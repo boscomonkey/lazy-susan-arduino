3006326e3dd35e8adc3b758540fdbdb70d6e6cd7
naffis <davidnaffis@gmail.com>
2014-11-21 09:40:02 -0500

    latest changes

--- servo-turning-sine-wave.ino
+++ servo-turning-sine-wave.ino
@@ -14,21 +14,23 @@ void setup()
 {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
   myservo.write(90);
+
+  Serial.begin(9600);      // open the serial port at 9600 bps:    
 }
 
 void loop()
 {
-  const int startAngle = 10;
-  const int endAngle = 170;
+  const int startAngle = 20;
+  const int endAngle = 130;
 
   // milliseconds to wait between servo movement
-  const int delayTime = 25;
+  const int delayTime = 30;
 
   // maximum servo movement per iteration
   const int increment = 3;
 
   // milliseconds to wait at one end point before reversing rotation
-  const int turnaroundTime = 25;
+  const int turnaroundTime = 10;
 
   int angle = 0;
 
@@ -39,12 +41,7 @@ void loop()
     myservo.write(angle);       // tell servo to go to position in variable 'angle'
     delay(delayTime);           // waits for the servo to reach the position
 
-    // amount to rotate head depends on where it is: more in the
-    // middle, & less at the ends. AKA sine wave.
-    float rad = angle * degrees_to_radians;
-    int increAngle = ceil(increment * sin(rad));
-
-    angle += increAngle;
+    angle += calcDeltaAngle(angle, startAngle, endAngle, increment);
   }
 
   delay(turnaroundTime);
@@ -54,10 +51,35 @@ void loop()
     myservo.write(angle);       // tell servo to go to position in variable 'angle'
     delay(delayTime);           // waits for the servo to reach the position
 
-    float rad = angle * degrees_to_radians;
-    int increAngle = ceil(increment * sin(rad));
-
     // note decrement instead of increment
-    angle -= increAngle;
+    angle -= calcDeltaAngle(angle, startAngle, endAngle, increment);
   }
 }
+
+int calcDeltaAngle(int angle, int startAngle, int endAngle, int maxAngleIncrement)
+{
+  // normal half circle is 180 degrees
+  const double wholeRange = 180.0;
+
+  // amount to rotate head depends on where it is: more in the
+  // middle, & less at the ends. AKA sine wave.
+  int currRange = endAngle - startAngle;
+  double rangeRatio = wholeRange / currRange;
+  int offsetAngle = angle - startAngle;
+  double normalizedOffset = rangeRatio * offsetAngle;
+
+  double rad = normalizedOffset * degrees_to_radians;
+  double sineValue = sin(rad);
+  int increAngle = ceil(0.1 + maxAngleIncrement * sineValue);
+
+  /*
+  Serial.print(angle);
+  Serial.print("\t");
+  Serial.print(increAngle);
+  Serial.println("");
+  */
+
+  return increAngle;
+}
+
+
