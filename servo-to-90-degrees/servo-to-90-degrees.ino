53701cec7dc6d507fa1a699ecf23ff5b9758077b
Bosco So <bso@alum.mit.edu.nospam>
2015-07-04 14:15:58 -0400

    oscillate servo side-to-side 1 degree at a time (30 - 150)

--- servo-to-90-degrees/servo-to-90-degrees.ino
+++ servo-to-90-degrees/servo-to-90-degrees.ino
@@ -8,10 +8,15 @@ Servo myservo;  // create servo object to control a servo
 // multiply by degrees to get radians
 //
 const double degrees_to_radians = 0.0174532925;
+const int DELAY_MS = 20;
+const int DEGREE_INCREMENT = 1;
+int degree = 90;
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  myservo.write(90);
+  myservo.write(degree);
+
+  Serial.begin(9600);
 
   // initialize digital pin 13 as an output.
   pinMode(13, OUTPUT);
@@ -19,7 +24,25 @@ void setup() {
 
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
+
+  Serial.print(degree);
+  for (; degree < 150 ; degree += DEGREE_INCREMENT) {
+    Serial.print(".");
+
+    myservo.write(degree);
+    delay(DELAY_MS);
+  }
+  Serial.print("\n");
+
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);               // wait for a second
+
+  Serial.print(degree);
+  for (; degree > 30 ; degree -= DEGREE_INCREMENT) {
+    Serial.print(".");
+
+    myservo.write(degree);
+    delay(DELAY_MS);
+  }
+  Serial.print("\n");
+
 }
